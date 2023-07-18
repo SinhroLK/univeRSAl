1c6ba61ae25800b662efde0baa6f9d3baa548141
SinhroLK <lkrstic001@gmail.com>
2023-07-18 11:08:46 +0200

    Added some ascii art and a basic menu

--- server-client/server.py
+++ server-client/server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import random
 
 FORMAT = 'utf-8'
 HEADER = 1024
@@ -9,16 +10,67 @@ serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind((serverName, serverPort))
 serverSocket.listen()
 
+def smallE():
+    pass
 
-def handleClient(connection, addr):
+def hastad():
+    pass
+def commonModulus():
+    pass
+def wiener():
+    pass
+def boneh():
+    pass
+def configure(connection, addr):
+    print(f'New connection at {addr}')
+    print(r"""                                          
+     ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄                                      
+    ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌                                     
+    ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌                                     
+    ▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌                                     
+    ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌                                     
+    ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌                                     
+    ▐░█▀▀▀▀█░█▀▀  ▀▀▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌                                     
+    ▐░▌     ▐░▌            ▐░▌▐░▌       ▐░▌                                     
+    ▐░▌      ▐░▌  ▄▄▄▄▄▄▄▄▄█░▌▐░▌       ▐░▌                                     
+    ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌                                     
+     ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀                                      
+                                                                                
+     ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄    ▄               ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ 
+    ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌             ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+    ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░▌   ▐░▌           ▐░▌ ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌
+    ▐░▌          ▐░▌       ▐░▌▐░▌    ▐░▌         ▐░▌  ▐░▌          ▐░▌       ▐░▌
+    ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌       ▐░▌▐░▌     ▐░▌       ▐░▌   ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌
+    ▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌      ▐░▌     ▐░▌    ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+     ▀▀▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌▐░▌       ▐░▌   ▐░▌     ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀█░█▀▀ 
+              ▐░▌▐░▌       ▐░▌▐░▌        ▐░▌ ▐░▌      ▐░▌          ▐░▌     ▐░▌  
+     ▄▄▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄▐░▐░▌       ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌      ▐░▌ 
+    ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌        ▐░░░░░░░░░░░▌▐░▌       ▐░▌
+     ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀          ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀                                                                                                                                                                                                                    
+                        """)
     while True:
-        prom = connection.recv(HEADER).decode(FORMAT)
-        print(prom)
+        print("1. Small e attack")
+        print("2. Hastad broadcast attack")
+        print("3. Common modulus attack")
+        print("4. Wiener attack")
+        print("5. Boneh-Durfee Attack")
+        msg = input("Choose your desired configuration")
+        #msg = random.randint(1, 5)
+        match msg:
+            case "1": smallE()
+            case "2": hastad()
+            case "3": commonModulus()
+            case "4": wiener()
+            case "5": boneh()
+            case _: print("Unknown configuration, please choose an existing one")
 
 
 if __name__ == "__main__":
     print('Server is ready')
     while True:
-        connectionSocket, address = serverSocket.accept()
-        thread_main = threading.Thread(target=handleClient, args=(connectionSocket, address,))
-        thread_main.start()
+        try:
+            connectionSocket, address = serverSocket.accept()
+            thread_main = threading.Thread(target=configure, args=(connectionSocket, address,))
+            thread_main.start()
+        except:
+            print("Server killed")
